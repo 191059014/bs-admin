6ec8339192a8d89eed8945fc0be8c6903980b0ef
黄彪 <191059014@qq.com>
2021-01-29 15:03:42 +0800

    系统设置

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,31 @@
 <template>
-  <el-form
-    :model="AgentDO"
-    ref="AgentDO"
-    label-position="left"
-    label-width="0px"
-    class="login-container"
-  >
-    <h3 class="title">系统登录</h3>
-    <el-form-item prop="usernameOrMobile">
-      <el-input type="text" v-model="AgentDO.usernameOrMobile" auto-complete="off" placeholder="用户名或手机号"></el-input>
-    </el-form-item>
-    <el-form-item prop="password">
-      <el-input type="password" v-model="AgentDO.password" auto-complete="off" placeholder="密码"
-                show-password></el-input>
-    </el-form-item>
-    <el-form-item style="width:100%;padding-top:20px;">
-      <el-button
-        type="primary"
-        style="width:100%;"
-        @click="doLogin"
-        :loading="logining"
-      >登录
-      </el-button>
-    </el-form-item>
-  </el-form>
+  <div class="login_wapper">
+    <el-form
+      :model="AgentDO"
+      ref="AgentDO"
+      label-position="left"
+      label-width="0px"
+      class="login-container"
+    >
+      <h3 class="title">系统登录</h3>
+      <el-form-item prop="usernameOrMobile">
+        <el-input type="text" v-model="AgentDO.usernameOrMobile" auto-complete="off" placeholder="用户名或手机号"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input type="password" v-model="AgentDO.password" auto-complete="off" placeholder="密码"
+                  show-password></el-input>
+      </el-form-item>
+      <el-form-item style="width:100%;padding-top:20px;">
+        <el-button
+          type="primary"
+          style="width:100%;"
+          @click="doLogin"
+          :loading="logining"
+        >登录
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -58,6 +60,11 @@
 </script>
 
 <style scoped>
+
+  body {
+    background: url("../../static/image/login_bg.jpg") no-repeat 0 0!important;
+  }
+
   .login-container {
     /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
     -webkit-border-radius: 5px;
@@ -73,7 +80,7 @@
   }
 
   .title {
-    margin: 0px auto 40px auto;
+    margin: 0 auto 40px auto;
     text-align: center;
     color: #505458;
   }
